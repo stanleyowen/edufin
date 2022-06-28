07fe32d1f9cd1f8c60568a559a4ae317a2bff1db
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-06-28 22:16:59 +0700

    import sign in component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,14 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import SignIn from "./components/signIn";
 import Navbar from "./components/navbar";
 
 const Home: NextPage = () => {
   return (
     <>
       <Navbar />
-      <div
-        className="dark:bg-slate-800 dark:text-slate-400"
-        style={{ height: "900px" }}
-      >
-        hi
-      </div>
+      <SignIn />
     </>
   );
 };
