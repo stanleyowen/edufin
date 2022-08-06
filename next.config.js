3f314bc08a4eb8588c662d286e46f924ebec6ce9
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2022-08-06 23:14:03 +0700

    i18n: indonesian language

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ["user-images.githubusercontent.com"]
-    }
+        domains: ["user-images.githubusercontent.com"],
+    },
+    i18n: {
+        locales: ["en-US", "id-ID"],
+        defaultLocale: "en-US",
+    },
 };
 
 module.exports = nextConfig;
